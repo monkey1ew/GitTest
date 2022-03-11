36ec0383143247ee90833440989dfcab5d3ccc3a
Fqh <Fqh@qq.com>
2022-03-11 20:40:31 +0800

    first commit

---
+++ src/main/java/com/fqh/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.fqh.git;
+
+/**
+ * @author 海盗狗
+ * @version 1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git");
+    }
+}
